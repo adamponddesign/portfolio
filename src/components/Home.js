9410227e7a91f7205bb9300a0577330041cafb1e
Adam Pond <adam.pond@hotmail.co.uk>
2019-06-27 02:16:52 +0100

    added logo animations" "

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
 
 class Home extends React.Component {
   render() {
@@ -6,12 +8,16 @@ class Home extends React.Component {
       <section className="hero is-fullheight">
         <div className="hero-body">
           <div className="container">
-            <h1>
+
+            <h1 id="name-home-hero">
               <span className="brackets">{'<'}</span>
               <span>{'Adam Pond /'}</span>
               <span className="brackets">{'>'}</span>
             </h1>
-            <h2 id="logo-sub">Full-Stack Developer</h2>
+            <h2 id="logo-sub" className="anim-typewriter">Full-Stack Developer</h2>
+            <div id="scroll-chevron" className="fade-in bounce">
+              <FontAwesomeIcon icon={faChevronDown} />
+            </div>
 
           </div>
         </div>
